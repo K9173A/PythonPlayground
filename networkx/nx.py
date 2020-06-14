11b9ac7a9a9382515b28b62bc0d5ed5632f5f5d6
K9173A <k9173a@gmail.com>
2020-06-14 18:57:51 +0300

    Изучение networkx

---
+++ networkx/nx.py
@@ -0,0 +1,26 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+graph = nx.Graph()
+
+# Добавление вершин графа
+graph.add_weighted_edges_from([
+    ('a', 'b', 0.9),
+    ('b', 'c', 0.5),
+    ('a', 'c', 0.5),
+    ('c', 'd', 1.2)
+])
+
+# Алгоритм Дейкстры (кротчайший маршрут от `a` до `d`.
+print(nx.dijkstra_path(graph, 'a', 'd'))  # ['a', 'c', 'd']
+
+nx.draw(
+    graph,
+    pos=nx.spectral_layout(graph),
+    nodecolor='g',
+    edge_color='b'
+)
+
+# Для отображения графа нужен numpy и matplotlib/graphviz.
+plt.show()
