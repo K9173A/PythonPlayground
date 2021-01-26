e1b2ce20de772f578784124a8903cb2e45313e0f
Andrew <k9173a@gmail.com>
2021-01-27 00:27:33 +0300

    working filler for postgres

--- miscellaneous/db_filler.py
+++ miscellaneous/db_filler.py
@@ -2,6 +2,7 @@
 Генерация sql-скрипта с insert-командами.
 """
 import json
+import random
 import os
 import types
 
@@ -21,26 +22,35 @@ class FakeDataGenerator:
             i += 1
 
     @staticmethod
-    def generate_config():
-        return json.dumps({
-            'agent_target_first_answer_time': fake.pyint(min_value=1, max_value=120, step=1),
-            'auto_prioritization_by_service_level': fake.pybool(),
-            'target_service_level':  fake.pyint(min_value=0, max_value=100, step=1)
-        })
-
-    @staticmethod
     def generate_increment_name(name, from_=0, to=float('inf')):
         i = from_
         while i < to:
             yield '{}_{}'.format(name, i)
             i += 1
 
+    @staticmethod
+    def generate_config():
+        return json.dumps({
+            'agent_target_first_answer_time': fake.pyint(min_value=1, max_value=120, step=1),
+            'auto_prioritization_by_service_level': fake.pybool(),
+            'target_service_level': fake.pyint(min_value=0, max_value=100, step=1)
+        })
+
+    @staticmethod
+    def generate_bool():
+        return random.choice(['true', 'false'])
+
 
 def main(schema, table, configuration, limit):
     columns = ', '.join(configuration.keys())
 
-    rows = ['LOCK TABLE `{table}` IN ROW EXCLUSIVE;'.format(table=table),
-            'INSERT INTO `{table}` ({columns}) VALUES'.format(table=table, columns=columns)]
+    rows = [
+        'BEGIN WORK;',
+        'LOCK TABLE {schema}.{table} IN ROW EXCLUSIVE MODE;'.format(schema=schema, table=table),
+        'INSERT INTO {schema}.{table}'.format(schema=schema, table=table),
+        '\t({columns})'.format(columns=columns),
+        'VALUES'
+    ]
 
     for index in range(limit):
         row = []
@@ -53,16 +63,26 @@ def main(schema, table, configuration, limit):
             else:
                 result = generator
 
-            row.append(result if result == 'null' else json.dumps(result))
+            if isinstance(result, int):
+                result = str(result)
+            elif isinstance(result, str) and result not in ['NULL', 'true', 'false']:
+                result = '\'' + result + '\''
+            elif isinstance(result, dict):
+                result = "'" + str(result).replace('\'', '"') + "'"
+
+            row.append(result)
 
         string = '\t({})'.format(', '.join(row))
         string += ',' if index < limit - 1 else ';'
 
         rows.append(string)
 
-    rows.append('UNLOCK TABLES;')
+    rows.append('COMMIT WORK;')
 
     path = os.path.join(os.path.dirname(__file__), '{schema}-{table}.sql'.format(schema=schema, table=table))
+
+    os.remove(path)
+
     with open(path, mode='w') as f:
         f.write('\n'.join(rows))
 
@@ -73,11 +93,11 @@ if __name__ == '__main__':
          configuration={
              'departmentid': FakeDataGenerator.generate_increment(from_=12),
              'departmentkey': lambda: fake.company(),
-             'departmenorder': FakeDataGenerator.generate_increment(from_=12),
-             'departmentgeo': 'null',
-             'isprivate': 0,
-             'ishidden': 0,
+             'departmentorder': FakeDataGenerator.generate_increment(from_=12),
+             'departmentgeo': 'NULL',
+             'isprivate': 'false',
+             'ishidden': 'false',
              'config': FakeDataGenerator.generate_config(),
-             'deleted': 0
+             'deleted': 'false'
          },
-         limit=10)
+         limit=100)
